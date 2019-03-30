c1cdacf5d664c6289d3fd1b1be35c03afc7a371d
ritupravakar <ritupravarar22@gmail.com>
2019-03-30 10:58:53 +0530

    initial commit

---
+++ src/app/score-card/score-card -component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core'
+
+@Component({
+
+  selector:'app-score-card',
+  template: '<div>0</div><div>-</div> <div>0</div>',
+  styleUrls:['./score-card-components.css']
+})
+
+export class ScoreCardComponent{
+
+
+}
